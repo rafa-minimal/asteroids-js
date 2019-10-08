7ed38377c5275f76c0fa3a59dae3272d7c9eb55c
Rafal Golcz <rgl@touk.pl>
2019-10-08 14:49:40 +0200

    Looks like css-loader needs to be installed as well, use contenthash for js bundle

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'asteroids.bundle.js'
+    filename: 'asteroids.[contenthash].js'
   },
   module: {
     rules: [
