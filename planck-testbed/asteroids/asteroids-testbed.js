b169e5032e5fc63b78c531c7085db99ccbe22be5
Rafal Golcz <rgl@touk.pl>
2019-10-08 14:21:26 +0200

    Move asteroids-testbed to separate directory

---
+++ planck-testbed/asteroids/asteroids-testbed.js
@@ -0,0 +1,325 @@
+planck.testbed('Asteroids', function(testbed) {
+
+    testbed.speed = 1;
+    testbed.hz = 50;
+
+    const pl = planck;
+    const Vec2 = pl.Vec2;
+    const world = new pl.World({
+        gravity : Vec2(0, 0)
+    });
+    const toDestroy = [];
+
+    // todo: do przetestowania, osobny moduł (Scheduler)
+    const scheduledEvents = [];
+    function schedule(worldTimeMs, action) {
+        const event = {
+            worldTimeMs: worldTimeMs,
+            action: action
+        };
+        for(let i = 0; i < scheduledEvents.length; i++) {
+            if (scheduledEvents[i].worldTimeMs > worldTimeMs ) {
+                scheduledEvents.splice(i, 0, event)
+            }
+        }
+        scheduledEvents.push(event)
+    }
+
+    const asteroidCat = 0b00001;
+    const rocketCat =   0b00010;
+    const bulletCat =   0b00100;
+    const edgeCat =     0b01000;
+
+    const ASTEROID_NUM_VERTICES = [null, 5, 6, 6, 8];
+    const ASTEROID_RADIUS = [null, 0.7, 1, 1.4, 2];
+    const ASTEROID_ENERGY = [null, 10, 20, 40, 60];
+    const ASTEROID_PIECES = [null, 0, 3, 3, 3];
+    const ASTEROID_MAX_SPEED = 4.0;
+    const ASTEROID_MAX_OMEGA = 2.0;
+
+    const EDGE_FORCE_FACTOR = 0.4;
+
+    const ROCKET_BULLET_VELOCITY = 25;
+    const ROCKET_THRUST = 5.0;
+    const DEFAULT_ROCKET_LINEAR_DAMPING = 0.2;
+
+    // math
+    function rnd(upper, lower) {
+        var l = lower || 0;
+        var r = upper - l;
+        return Math.random() * r + l
+    }
+
+    rotateDeg = (vec, angleDeg) => {
+        let rad = angleDeg / (2 * Math.PI);
+        let c = Math.cos(angleDeg);
+        let s = Math.sin(angleDeg);
+        vec.x = c * vec.x - s * vec.y;
+        vec.y = s * vec.x + c * vec.y;
+        return vec;
+    };
+
+    function rndVecRadius(radius) {
+        const angle = rnd(360.0);
+        return Vec2(radius * Math.cos(angle), radius * Math.sin(angle));
+    }
+
+    // world
+    const worldRadius = 20;
+    const worldRadiusSquared = worldRadius ** 2;
+
+    // rocket
+    const rocketHolder = {instance: null};
+    function createRocket() {
+        const rocket = world.createDynamicBody({dynamicDamping: DEFAULT_ROCKET_LINEAR_DAMPING});
+
+        rocket.createFixture(pl.Polygon([
+            Vec2(-1, -1),
+            Vec2( 0,  1),
+            Vec2( 1, -1)
+        ]), {
+            density: 0.5,
+            friction: 0.5,
+            filterCategoryBits: rocketCat,
+            filterMaskBits: edgeCat | rocketCat | asteroidCat
+        });
+        // MassData is not exported
+        // const massData = pl.MassData();
+        const massData = {center: Vec2()};
+        rocket.getMassData(massData);
+        massData.center.setZero();
+        rocket.setMassData(massData);
+
+        rocket.rocket = true;
+        rocket.nextBullet = 0;
+        rocket.energy = 10;
+        rocket.postSolve = (contact, impulse, self, other) => {
+            let sum = impulse.normalImpulses.reduce((sum, impulse) => (sum || 0) + impulse);
+            self.energy -= sum;
+            if (self.energy <= 0) {
+                toDestroy.push(self);
+            }
+        };
+        rocket.beforeDestroy = (self) => {
+            rocketHolder.instance = null;
+            const securityCircle = world.createBody();
+            securityCircle.createFixture(pl.Circle(3), {
+                filterCategoryBits: asteroidCat,
+                filterMaskBits: asteroidCat
+            });
+            // todo: czas dziwnie szybko upływa
+            schedule(worldTimeMs + 5000, () => world.destroyBody(securityCircle));
+            schedule(worldTimeMs + 5000, () => createRocket())
+        };
+        rocketHolder.instance = rocket;
+    }
+    createRocket();
+
+    /* The time [sec] after which angular velocity reaches 64% of max value */
+    const angularTau = 0.11;
+    /** Max angular velocity of rocket rad/s */
+    const maxAngularVel = 4.5;
+
+    const nominalAngDamping = 1.0/angularTau;
+    const maxTorque = maxAngularVel * nominalAngDamping * rocketHolder.instance.getInertia();
+
+    // asteroid
+    function createAsteroid(level, pos, vel) {
+        pos = pos || rndVecRadius(rnd(worldRadius));
+        vel = vel || rndVecRadius(ASTEROID_MAX_SPEED);
+
+        const asteroid = world.createDynamicBody({
+            position: pos,
+            linearVelocity: vel,
+            angle: rnd(360),
+            angularVelocity: rnd(-ASTEROID_MAX_OMEGA, ASTEROID_MAX_OMEGA),
+            linearDamping: 0.01,
+            angularDamping: 0.01
+        });
+
+        const len = ASTEROID_NUM_VERTICES[level];
+        const radius = ASTEROID_RADIUS[level];
+        const vertices = Array(len);
+        for (let i = 0; i < len; i++) {
+            let rad = rnd(radius * 0.6, radius * 1.4);
+            let angle = 2 * Math.PI * i / len;
+            vertices[i] = Vec2(rad * Math.cos(angle), rad * Math.sin(angle));
+        }
+
+        asteroid.createFixture(pl.Polygon(vertices), {
+            density: 1.0,
+            restitution:0.9,
+            friction: 0.2,
+            filterCategoryBits: asteroidCat,
+            filterMaskBits: edgeCat | rocketCat | bulletCat | asteroidCat
+        });
+        asteroid.asteroid = true;
+        asteroid.level = level;
+        asteroid.energy = ASTEROID_ENERGY[level];
+        asteroid.beforeDestroy = function(self) {
+            if (self.level > 0) {
+                let r = ASTEROID_RADIUS[self.level] / 2;
+                let numPieces = ASTEROID_PIECES[self.level];
+                let pos = rotateDeg(Vec2(0, r), rnd(360));
+                for (let i = 0; i < numPieces; i++) {
+                    createAsteroid(level - 1, self.getPosition().clone().add(pos), self.getLinearVelocity());
+                    pos = rotateDeg(pos, 360 / numPieces);
+                }
+            }
+        };
+        return asteroid;
+    }
+
+    for (let i = 0; i < 10; i++) {
+        createAsteroid(Math.floor(rnd(1, 5)));
+    }
+
+    function spawnAsteroid() {
+        createAsteroid(Math.floor(rnd(1, 5)));
+        schedule(worldTimeMs + 1000, spawnAsteroid);
+    }
+
+    schedule(1000, function () {
+        spawnAsteroid();
+    });
+
+    // edge force
+    const edge = world.createBody();
+    edge.createFixture(pl.Circle(worldRadius), {
+        isSensor: true,
+        filterCategoryBits: edgeCat,
+        filterMaskBits: rocketCat | asteroidCat
+    });
+    function applyEdgeForce() {
+        for (let body = world.getBodyList(); body; body = body.getNext()) {
+            if (body.asteroid || body.rocket) {
+                const lengthSquared = body.getPosition().lengthSquared();
+                const outside = lengthSquared - worldRadiusSquared;
+                if (outside > 0) {
+                    const force = Vec2(body.getPosition());
+                    force.normalize();
+                    force.mul(-body.getMass() * outside * EDGE_FORCE_FACTOR);
+                    body.applyForceToCenter(force, false);
+                }
+
+                if (body.rocket) {
+                    body.setLinearDamping((outside > 0) && 1.0 || DEFAULT_ROCKET_LINEAR_DAMPING)
+                }
+            }
+        }
+    }
+
+    // bullets
+    function createBullet(pos, vel) {
+        const bullet = world.createDynamicBody({
+            position: pos,
+            linearVelocity: vel,
+            fixedRotation: true
+        });
+        bullet.createFixture(pl.Circle(0.1), {
+            density: 2,
+            restitution: 1,
+            filterCategoryBits: bulletCat,
+            filterMaskBits: asteroidCat
+        });
+        bullet.bullet = true;
+        bullet.deadTimeMs = worldTimeMs + 5000;
+        bullet.beginContact = (contact, self, other) => {
+            if (other.energy) {
+                other.energy -= 10;
+                if (other.energy <= 0) {
+                    toDestroy.push(other);
+                }
+            }
+            toDestroy.push(self);
+        };
+        bullet.beforeDestroy = (self) => {
+            // todo: emit particles
+        }
+    }
+
+    world.on('begin-contact', (contact) => {
+        let bodyA = contact.getFixtureA().getBody();
+        let bodyB = contact.getFixtureB().getBody();
+        if (bodyA.beginContact) {
+            bodyA.beginContact(contact, bodyA, bodyB);
+        }
+        if (bodyB.beginContact) {
+            bodyB.beginContact(contact, bodyB, bodyA);
+        }
+    });
+
+    world.on('post-solve', (contact, impulse) => {
+        let bodyA = contact.getFixtureA().getBody();
+        let bodyB = contact.getFixtureB().getBody();
+        if (bodyA.postSolve) {
+            bodyA.postSolve(contact, impulse, bodyA, bodyB);
+        }
+        if (bodyB.postSolve) {
+            bodyB.postSolve(contact, impulse, bodyB, bodyA);
+        }
+    });
+
+    testbed.keydown = function() {
+        if (testbed.activeKeys.down) {
+
+        } else if (testbed.activeKeys.up) {
+
+        }
+    };
+
+    let worldTimeMs = 0;
+
+    testbed.step = function(dt) {
+        worldTimeMs += dt;
+        if (rocketHolder.instance) {
+            const rocket = rocketHolder.instance
+            if (testbed.activeKeys.right && !testbed.activeKeys.left) {
+                rocket.setAngularDamping(nominalAngDamping);
+                rocket.applyTorque(-maxTorque, true);
+            } else if (testbed.activeKeys.left && !testbed.activeKeys.right) {
+                rocket.setAngularDamping(nominalAngDamping);
+                rocket.applyTorque(maxTorque, true);
+            } else {
+                rocket.setAngularDamping(nominalAngDamping * 3);
+            }
+            if (testbed.activeKeys.up) {
+                const force = rocket.getWorldVector(Vec2(0, ROCKET_THRUST));
+                rocket.applyForceToCenter(force, true);
+            }
+            if (testbed.activeKeys.fire && worldTimeMs >= rocket.nextBullet) {
+                const pos = rocket.getWorldPoint(Vec2(0, 1.52));
+                const vel = rocket.getWorldVector(Vec2(0, ROCKET_BULLET_VELOCITY));
+                createBullet(pos, vel);
+                rocket.nextBullet = worldTimeMs + 200
+            }
+        }
+
+        applyEdgeForce();
+        for (let body = world.getBodyList(); body; body = body.getNext()) {
+            if (body.deadTimeMs && worldTimeMs > body.deadTimeMs) {
+                toDestroy.push(body)
+            }
+        }
+        while(toDestroy.length > 0) {
+            let ent = toDestroy.pop();
+            if (ent.beforeDestroy) {
+                ent.beforeDestroy(ent)
+            }
+            world.destroyBody(ent)
+        }
+        while(scheduledEvents.length > 0 && worldTimeMs >= scheduledEvents[0].worldTimeMs) {
+            scheduledEvents.pop().action();
+        }
+
+        if (rocketHolder.instance) {
+            const pos = rocketHolder.instance.getPosition();
+            testbed.x = pos.x;
+            testbed.y = -pos.y;
+        }
+    };
+
+    testbed.info('‹/›: rotate, ^: Accelerate, A: Fire');
+
+    return world;
+});
